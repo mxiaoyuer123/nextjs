8b72d959685fd8fdbd09a61ac2f435ba54c78493
mxiaoyuer123 <64115876+mxiaoyuer123@users.noreply.github.com>
2021-05-20 21:13:29 +0800

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -1,6 +1,6 @@
 export default {
   repository: 'https://github.com/shuding/nextra',
-  titleSuffix: ' – Nextra',
+  titleSuffix: ' – XTA',
   logo: (
     <>
       <span className="mr-2 font-extrabold hidden md:inline">XTA</span>
